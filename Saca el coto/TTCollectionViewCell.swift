dd88c360cbdbc7ccecd29d747fdc28f88333d969
Héctor Cuevas Morfín <poncho@Hectors-MacBook-Pro-2.local>
2016-09-05 18:18:26 -0500

    TTCollectionviewcell added

---
+++ Saca el coto/TTCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TTCollectionViewCell.swift
+//  Saca el coto
+//
+//  Created by Héctor Cuevas Morfín on 9/5/16.
+//  Copyright © 2016 AppData. All rights reserved.
+//
+
+import UIKit
+
+class TTCollectionViewCell: UICollectionViewCell {
+    
+}
